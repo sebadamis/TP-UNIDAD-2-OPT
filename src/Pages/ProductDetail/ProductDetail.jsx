bf7840ced86de8e9ca32d1c6c3a5dd79391db105
Sebastián Damis <sebastian.damis@gmail.com>
2025-05-23 20:40:14 -0300

    3rd

--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-import { getProdById } from '../../services/prodservices';
+import { getProdById } from '../../services/prodServices';
 // import ProdDetailCard2 from '../../components/ProdDetailCard2/ProdDetailCard2';
 import ProdDetailCard from '../../components/ProdDetailCard/ProdDetailCard';
 
